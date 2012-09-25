aab73f351c0a78d6a95d49d6268da7caf81213b3
Lonnen <chris.lonnen@gmail.com>
2012-09-24 20:54:29 -0700

    kitten edition

--- js/graphdash.js
+++ js/graphdash.js
@@ -3,15 +3,28 @@ $(function(){ //DOM Ready
     // Non-Mozilla deploys should change this.
     GRAPHITE_HOST = "https://graphite-phx.mozilla.org/"
 
+    // temp var for testing
+    default_graph = Graphite({'host': GRAPHITE_HOST})
+        .targets(["sumSeries(stats.socorro-prod.socorro.processors.*.jobs)"])
+        .width(800)
+        .height(430)
+        .url()
+
+    var newWidget = function() {
+        widget = grid.add_widget('<li class="gs_w new"></li>',
+            Math.round(Math.random() * 3) + 1, 
+            Math.round(Math.random() * 2) + 1)
+        widget.css('background-image', 
+            'url(http://placekitten.com/'+widget.width()+'/'+widget.height())
+    }
+
+
     var grid = $(".grid ul").gridster({
         widget_margins: [10, 10],
         widget_base_dimensions: [140, 140]
     }).data('gridster');
 
-    $("#new-graph").on('click', function(event) {
-        grid.add_widget('<li class="gs_w new"></li>', 1, 1)
-        console.log('widget');
-    })
-
+    $("#new-graph").on('click', newWidget)
+    for (var i = 0; i < 6; i++) { newWidget(); }
 });
 
